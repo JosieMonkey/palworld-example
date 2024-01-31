7d65f46173ac70b44d308177116c279ceef4cf0f
JosieH <josepha.e.herrmann@gmail.com>
2024-01-31 14:35:32 -0500

    filled out pals in deck, shows all pals, shows random pal

--- src/mock-data.ts
+++ src/mock-data.ts
@@ -1,3 +1,104 @@
 import { Pal } from "./models";
 
-export const PALDECKS: Pal[] = [];
+export const Lamball: Pal = {
+  image:
+    "https://github.com/JosieMonkey/palworld-example/blob/main/assets/Lamball.png?raw=true",
+  name: "Lamball",
+  number: "001",
+  type1: "Neutral",
+  partnerSkill: "Fluffy Shield",
+};
+export const Cattiva: Pal = {
+  image:
+    "https://github.com/JosieMonkey/palworld-example/blob/main/assets/cattiva.png?raw=true",
+  name: "Cattiva",
+  number: "002",
+  type1: "Neutral",
+  partnerSkill: "Cat Helper",
+};
+
+export const Chikipi: Pal = {
+  image:
+    "https://github.com/JosieMonkey/palworld-example/blob/main/assets/chikipi.png?raw=true",
+  name: "Chikipi",
+  number: "003",
+  type1: "Neutral",
+  partnerSkill: "Egg Layer",
+};
+
+export const Lifmunk: Pal = {
+  image:
+    "https://github.com/JosieMonkey/palworld-example/blob/main/assets/lifmunk.png?raw=true",
+  name: "Lifmunk",
+  number: "004",
+  type1: "Grass",
+  partnerSkill: "Lifmunk Recoil",
+};
+
+export const Foxparks: Pal = {
+  image:
+    "https://github.com/JosieMonkey/palworld-example/blob/main/assets/foxparks.png?raw=true",
+  name: "Foxparks",
+  number: "005",
+  type1: "Fire",
+  partnerSkill: "Huggy Fire",
+};
+
+export const Fuack: Pal = {
+  image:
+    "https://github.com/JosieMonkey/palworld-example/blob/main/assets/fuack.png?raw=true",
+  name: "Fuack",
+  number: "006",
+  type1: "Water",
+  partnerSkill: "Surfing Slam",
+};
+
+export const Sparkit: Pal = {
+  image:
+    "https://github.com/JosieMonkey/palworld-example/blob/main/assets/sparkit.png?raw=true",
+  name: "Sparkit",
+  number: "007",
+  type1: "Electric",
+  partnerSkill: "Static Electricity",
+};
+
+export const Tanzee: Pal = {
+  image:
+    "https://github.com/JosieMonkey/palworld-example/blob/main/assets/tanzee.png?raw=true",
+  name: "Tanzee",
+  number: "008",
+  type1: "Grass",
+  partnerSkill: "Cheery Rifle",
+};
+
+export const Rooby: Pal = {
+  image:
+    "https://github.com/JosieMonkey/palworld-example/blob/main/assets/rooby.png?raw=true",
+  name: "Rooby",
+  number: "009",
+  type1: "Fire",
+  partnerSkill: "Tiny Spark",
+};
+
+export const Pengullet: Pal = {
+  image:
+    "https://github.com/JosieMonkey/palworld-example/blob/main/assets/pengullet.png?raw=true",
+  name: "Pengullet",
+  number: "010",
+  type1: "Water",
+  type2: "Ice",
+  partnerSkill: "Pengullet Cannon",
+};
+
+export const PALDECKS: Pal[] = [
+  Lamball,
+  Cattiva,
+  Chikipi,
+  Lifmunk,
+  Foxparks,
+  Fuack,
+  Sparkit,
+  Tanzee,
+  Rooby,
+  Pengullet,
+];
